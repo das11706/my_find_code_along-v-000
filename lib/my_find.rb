b80bee30396c4760785778fcc8710ffbd2fd9736
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-22 00:55:43 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_find(collection)
-
+  i=0
+  while i < collection.length
+    correct_element_block = []
+    correct_element_block << yield(collection[i])
+    i+=1
+  end
 end
